15cce533805d4645194bcf4efd0803651823dc58
jhy0409 <inooph@naver.com>
2021-08-12 14:05:17 +0900

    bst_26th / 4-3) 오토 레이아웃 구현하기(인터페이스 빌더)

--- project1_MusicPlayer/MusicPlayer1_스토리보드 구현/MusicPlayer/ViewController.swift
+++ project1_MusicPlayer/MusicPlayer1_스토리보드 구현/MusicPlayer/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController,AVAudioPlayerDelegate {
         // MARK: Life Cycle
         override func viewDidLoad() {
             super.viewDidLoad()
-            self.addViewsWithCode()
+            // self.addViewsWithCode()
             self.initializePlayer()
         }
 
