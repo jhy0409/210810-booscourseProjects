f3d6afe89082bfdb5906fad341fb21f064cabb8d
jhy0409 <inooph@naver.com>
2021-08-16 10:06:26 +0900

    bst_41th / 3-1) 뷰의 상태변화 감지 메서드

--- project2_ViewTransition/ViewTransition/ViewTransition/SecondViewController.swift
+++ project2_ViewTransition/ViewTransition/ViewTransition/SecondViewController.swift
@@ -11,21 +11,19 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func popToPrev() {
         self.navigationController?.popViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func dismissModal() {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    /*
+     push - pop : 네비게이션 컨트롤러
+     예시 ) 설정탭
+     
+     present - dismiss : 단순한 팝업, 입력폼
+     */
 }
