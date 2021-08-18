27cfc472faa4fe647eeca9bae9ba57ca20fb962a
jhy0409 <inooph@naver.com>
2021-08-18 20:11:46 +0900

    시작점 - 두번째 프로젝트 중간저장

---
+++ project2_LogIn/6_Project_LogIn/Project_LogIn/Project_LogIn/SignUpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewController.swift
+//  Project_LogIn
+//
+//  Created by inooph on 2021/08/18.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ project2_LogIn/6_Project_LogIn/Project_LogIn/Project_LogIn/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Project_LogIn
+//
+//  Created by inooph on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
