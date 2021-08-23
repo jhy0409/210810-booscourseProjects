87161a6240a33e38151c487be4d5c3987f88aae1
jhy0409 <inooph@naver.com>
2021-08-23 20:32:54 +0900

    bst_60th / 1-5) 테이블뷰에 동적으로 데이터 추가하기

--- project3_Weather/1_SimpleTable/SimpleTable/SimpleTable/ViewController.swift
+++ project3_Weather/1_SimpleTable/SimpleTable/SimpleTable/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
     let korean: [String] = ["가", "나", "다", "라", "마",  "바", "사", "아", "자", "차", "카", "타", "파", "하"]
     
     let english: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
+    // var dates: [Date] = []
+    var dates: [String] = []
+    let dateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        let today = Date()
+        
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        formatter.locale = Locale(identifier: "ko")
+        formatter.dateFormat = "yyyy-M-d H:m:s"
+        
+        return formatter
+    }()
+    
+    @IBAction func touchUpAddButton(_ sender: UIButton) {
+        let dateStr = dateFormatter.string(from: Date())
+        dates.append(dateStr)
+        // self.tableView.reloadData() // 전체 다시 불러오기 때문에 비효율적
+        self.tableView.reloadSections(IndexSet(2...2), with: UITableView.RowAnimation.automatic)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,7 +46,7 @@ class ViewController: UIViewController {
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 3
     }
 }
 
@@ -36,6 +58,8 @@ extension ViewController: UITableViewDataSource {
             return korean.count
         case 1:
             return english.count
+        case 2:
+            return dates.count
         default:
             return 0
         }
@@ -44,16 +68,25 @@ extension ViewController: UITableViewDataSource {
     // 셀 그리는 것
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
-        let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
-        cell.textLabel?.text = text
+        
+        if indexPath.section < 2 {
+            let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+            cell.textLabel?.text = text
+        } else {
+            cell.textLabel?.text = dates[indexPath.row]
+        }
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return section == 0 ? "한글" : "영어"
+        if section < 2 {
+            return section == 0 ? "한글" : "영어"
+        }
+        return nil
     }
     
+    // 색변경 - 섹션 배경, 글자
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         if let hdView = view as? UITableViewHeaderFooterView {
             hdView.contentView.backgroundColor = .systemBlue
