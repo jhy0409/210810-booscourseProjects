5a6a77ec94c7fe139b059164bab5a4da5f448264
jhy0409 <inooph@naver.com>
2021-08-29 15:28:37 +0900

    bst_75th / 1-3) 사진첩에서 사진 삭제하기

--- project4_Album/1_PhotosExample/PhotosExample/PhotosExample/ViewController.swift
+++ project4_Album/1_PhotosExample/PhotosExample/PhotosExample/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 import Photos
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,PHPhotoLibraryChangeObserver {
+    
+    
 
     
     @IBOutlet weak var tableView: UITableView!
     var fetchResult: PHFetchResult<PHAsset>!
     let imageManager: PHCachingImageManager = PHCachingImageManager()
     let cellIdentifier: String = "cell"
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let asset: PHAsset = self.fetchResult[indexPath.row]
+            
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetChangeRequest.deleteAssets([asset] as NSArray)
+            }, completionHandler: nil)
+        }
+    }
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult) else { return }
+        fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
+        }
+    }
     
     func requestCollection() {
         let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum,
@@ -58,6 +83,8 @@ class ViewController: UIViewController, UITableViewDataSource {
         default:
             break
         }
+        
+        PHPhotoLibrary.shared().register(self)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
