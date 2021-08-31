dd50144667dc2c8c74ca765008d0ea43b1995b4f
jhy0409 <inooph@naver.com>
2021-08-31 11:34:27 +0900

    bst_89th / 5-6) 셀 커스터마이징

---
+++ project4_Album/3_FriendsCollection/FriendsCollection/FriendsCollection/FriendsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsCollectionViewCell.swift
+//  FriendsCollection
+//
+//  Created by inooph on 2021/08/31.
+//
+
+import UIKit
+
+class FriendsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+}
