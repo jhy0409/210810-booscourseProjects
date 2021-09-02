b9089e28343ec3281452c3d2e2a094668a0b76e4
jhy0409 <inooph@naver.com>
2021-09-02 21:01:29 +0900

    ㅇ - 세번째 뷰 띄우기 / Todo : 데이터 전달

---
+++ project4_Album/Project_Album/Project_Album/Project_Album_8 세번째뷰 띄우기/Project_Album/ViewController/ThirdDetailPhoto_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdDetailPhoto_ViewController.swift
+//  Project_Album
+//
+//  Created by inooph on 2021/09/02.
+//
+
+import UIKit
+
+class ThirdDetailPhoto_ViewController: UIViewController {
+    var stoaryboardId: String = "thirdView"
+    //스토리보드 아이디 thirdView
+    @IBOutlet weak var detailImgView: UIImageView!
+    @IBOutlet weak var toolbar: UIToolbar!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
