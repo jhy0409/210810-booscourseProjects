7927043133bd2476a00edcf3daafc0abf20d5e3a
jhy0409 <inooph@naver.com>
2021-09-06 17:38:04 +0900

    bst_100th / 5-2-2) 탭 인터페이스 구현해보기

---
+++ project5_BoxOffice/2_TapBarInterface/TapBarInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapBarInterface
+//
+//  Created by inooph on 2021/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
