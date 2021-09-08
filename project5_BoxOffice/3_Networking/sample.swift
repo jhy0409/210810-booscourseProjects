7417cb8e6ba8927673df511a495beb64f01ae79c
jhy0409 <inooph@naver.com>
2021-09-08 22:20:53 +0900

    ㅇ - 코드대조

---
+++ project5_BoxOffice/3_Networking/sample.swift
@@ -0,0 +1,17 @@
+//
+//  sample.swift
+//  Networking
+//
+//  Created by inooph on 2021/09/08.
+//
+
+import Foundation
+
+//{"results":
+//    [
+//    {"name":{"title":"Mr","first":"Nikolaj","last":"Hansen"},"email":"nikolaj.hansen@example.com","picture":{"large":"https://randomuser.me/api/portraits/men/20.jpg","medium":"https://randomuser.me/api/portraits/med/men/20.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/20.jpg"}},
+//    {"name":{"title":"Madame","first":"Paula","last":"Legrand"},"email":"paula.legrand@example.com","picture":{"large":"https://randomuser.me/api/portraits/women/90.jpg","medium":"https://randomuser.me/api/portraits/med/women/90.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/90.jpg"}}
+//    ,{"name":{"title":"Ms","first":"Mia","last":"Davies"},"email":"mia.davies@example.com","picture":{"large":"https://randomuser.me/api/portraits/women/52.jpg","medium":"https://randomuser.me/api/portraits/med/women/52.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/52.jpg"}}
+//    ]
+//    ,"info":{"seed":"be5049e99957b8b6","results":20,"page":1,"version":"1.3"}
+//}
