d95c684ad2ec102579a8427ffb1f2f056d8929ff
jhy0409 <inooph@naver.com>
2021-09-08 22:52:23 +0900

    ㅇ - 폴더 정리, 리퀘스트 작성중

---
+++ project5_BoxOffice/Project_BoxOffice/2_ 리퀘스트/Project_BoxOffice/BoxOffice/Request.swift
@@ -0,0 +1,29 @@
+//
+//  Request.swift
+//  BoxOffice
+//
+//  Created by inooph on 2021/09/08.
+//
+
+import Foundation
+
+let recieveMovieID: String = "DidRecieveMovies"
+let DidRecievedMoviesNotification: Notification.Name = Notification.Name(recieveMovieID)
+
+func requestMoovies() {
+    let testURL: String = "https://connect-boxoffice.run.goorm.io/"
+    guard let url: URL = URL(string: testURL) else { return }
+    
+    let session: URLSession = URLSession(configuration: .default)
+    let dataTask: URLSessionDataTask = session.dataTask(with: url) { (data: Data?, urlResponse: URLResponse?, error: Error?) in
+        print(error?.localizedDescription)
+        guard let data = data else { return }
+        do {
+            let apiResponse: MovieList =  try JSONDecoder().decode(MovieList.self, from: data)
+            NotificationCenter.default.post(name: DidRecievedMoviesNotification, object: nil, userInfo: ["movies":apiResponse.movies])
+        } catch let err {
+            print(err.localizedDescription)
+        }
+    }
+    dataTask.resume()
+}
