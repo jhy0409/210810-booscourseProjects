f13a66d4d1c30659fd96582e499cbe22d4b4c646
jhy0409 <inooph@naver.com>
2021-09-12 08:57:52 +0900

    별점 새로

---
+++ project5_BoxOffice/Project_BoxOffice/9_ReviewWrite_O/Project_BoxOffice/BoxOffice/Cell/ThirdOfThird_DirectorAndActor_TableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdOfThird_DirectorAndActor_TableViewCell.swift
+//  BoxOffice
+//
+//  Created by inooph on 2021/09/10.
+//
+
+import UIKit
+
+class ThirdOfThird_DirectorAndActor_TableViewCell: UITableViewCell {
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var actorLabel: UILabel!
+    
+    func update(_ movieDetail: MovieDetail) {
+        directorLabel.text = movieDetail.director
+        actorLabel.text = movieDetail.actor
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ project5_BoxOffice/Project_BoxOffice/9_ReviewWrite_O/Project_BoxOffice/BoxOffice/sampleJson.swift
@@ -0,0 +1,13 @@
+//
+//  sampleJson.swift
+//  BoxOffice
+//
+//  Created by inooph on 2021/09/08.
+//
+
+import Foundation
+
+//{"order_type":0,"movies":[{"thumb":"http://movie.phinf.naver.net/20171201_181/1512109983114kcQVl_JPEG/movie_image.jpg?type=m99_141_2","reservation_rate":35.5,"user_rating":7.98,"date":"2017-12-20","id":"5a54c286e8a71d136fb5378e","grade":12,"reservation_grade":1,"title":"신과함께-죄와벌"},
+//    {"thumb":"http://movie.phinf.naver.net/20171102_209/1509602233507BiJrs_JPEG/movie_image.jpg?type=m99_141_2","reservation_rate":1.93,"user_rating":9.2,"date":"2017-12-07","id":"5a54df5ee8a71d136fb53d75","grade":0,"reservation_grade":8,"title":"뽀로로 극장판 공룡섬 대모험"}
+//    ]
+//}
